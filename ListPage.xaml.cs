bb4d18f8ff3c6c4fbe49a3b8893e314e7bd54486
roxana ban <banroxana22@gmail.com>
2023-12-08 21:37:56 +0200

    Laborator9

--- ListPage.xaml.cs
+++ ListPage.xaml.cs
@@ -21,4 +21,33 @@ public partial class ListPage : ContentPage
         await App.Database.DeleteShopListAsync(slist);
         await Navigation.PopAsync();
     }
+    async void OnChooseButtonClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ProductPage((ShopList)
+       this.BindingContext)
+        {
+            BindingContext = new Product()
+        });
+
+    }
+    async void OnDeleteItemButtonClicked(object sender, EventArgs e)
+    {
+        if (BindingContext is Product prod)
+        {
+            await App.Database.DeleteProductAsync(prod);
+            await Navigation.PopAsync();
+        }
+    }
+
+
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        var shopl = (ShopList)BindingContext;
+
+        listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
+    }
+
+
 }
